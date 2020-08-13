4880fd497b1abfe9b1da97fa3af263a91bccb29c
SinaSafari <m.sinasafari@gmail.com>
2020-08-13 15:45:51 +0430

    expense stats page

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from expenses.views import index, add_expense, expense_edit, delete_expense, search_expenses
+from expenses.views import index, add_expense, expense_edit, delete_expense, search_expenses, expense_category_summary, stats_view
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('edit-expense/<int:id>', expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', delete_expense, name="expense-delete"),
     path('search-expenses', csrf_exempt(search_expenses), name="search_expenses"),
+    path('expense_category_summary', expense_category_summary, name="expense_category_summary"),
+    path('stats', stats_view, name="stats")
 ]
